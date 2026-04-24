670e1f1f9be0fefa98191e06d6a45c9d183a1c62
Phaneki <168158726+Phaneki@users.noreply.github.com>
2026-04-23 22:17:47 -0500

    Initial commit - ASP.NET Core MVC 8 con Identity y SQLite

---
+++ PortalAcademico.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortalAcademico.Web.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
